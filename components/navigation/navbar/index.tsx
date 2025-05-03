7856992359ceae596e2249555942d223cfc0f94e
Pavith-Amarnath <developerpavith@gmail.com>
2025-05-03 13:37:13 +0530

    Set up Light & Dark Theme

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,12 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import ModeToggle from "./ModeToggle";
 
 export default function NavBar() {
   return (
-    <nav className="flex-between">
-      <ModeToggle />
-      <h1 className="text-dark-100 dark:text-light-900 text-4xl font-space-grotesk font-semibold">
-        DevOverflow
-      </h1>
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 gap-5">
+      <Link href={"/"} className="flex items-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          alt="DevFlow Logo"
+          width={23}
+          height={23}
+        />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-brand">Flow</span>
+        </p>
+      </Link>
+      <p>Global Search</p>
+      <div className="flex-between gap-5">
+        <ModeToggle />
+      </div>
     </nav>
   );
 }
